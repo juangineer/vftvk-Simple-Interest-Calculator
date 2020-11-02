2f52ffeb90873ce8990d572f91a6e6e7e1f2b6bd
Juan Sandoval <juangineer@gmail.com>
2020-11-01 19:47:41 -0500

    Add validation of amount and year

--- script.js
+++ script.js
@@ -19,17 +19,39 @@ function printResult(p, r, t, a) {
     result.innerHTML = message;
 }
 
+function resetResult() {
+    var result = document.getElementById("result");
+    result.innerHTML = "";
+}
+
 // Computes simple interest
 function compute() {
-    // get values from html
-    var p = document.getElementById("principal").value;
+    var areInputsValid = true;
+    // get and validate principal
+    var principal = document.getElementById("principal");
+    var p = parseFloat(principal.value);
+    if (isNaN(p) || p < 0) {
+        areInputsValid = false;
+        principal.focus();
+    }
+    // get percentage / rate
     var r = document.getElementById("rate").value;
-    var t = Number(document.getElementById("years").value);
+    var years = document.getElementById("years");
+    var t = parseInt(years.value);
+    if (isNaN(t) || t < 0) {
+        areInputsValid = false;
+        years.focus();
+    }
     // calculate the simple interest
     var a = p * (r * t / 100.0);
     // round to cents
     a = Math.round(a * 100.0) / 100.0;
 
     // print the result into html
-    printResult(p, r, t, a);
+    if (areInputsValid) {
+        printResult(p, r, t, a);
+    }
+    else {
+        resetResult()
+    }
 }
